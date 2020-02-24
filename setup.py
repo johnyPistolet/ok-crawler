b45cb94c2c99e059166210a9d27f4e045d734bba
johnyPistolet <j(WRCnK/v0yO:a=;k;&9J'8G>
2020-02-25 01:22:31 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='odnoklassniki-crawler',
     version='0.0.1',
-    packages=[''],
+    packages=find_packages(),
     url='',
     license='Apache 2.0',
     author='user',
